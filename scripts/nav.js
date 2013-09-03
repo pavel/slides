e2fd9eda6cbebdd23e1575d573a2cab10953bbf2
pavel <p.varantsou@gmail.com>
2013-09-03 17:20:44 +0300

    Removed old unused line from nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,6 +2,5 @@
     router('go/:slide', function (slide) {
         $('.slide.current').removeClass('current');
         $('#' + slide).addClass('current');
-        $('[href="#home"]').show();
     });
 }(jQuery, routie));
