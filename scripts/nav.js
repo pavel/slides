882b4a3a40a6b0e621289d2e311ae463598ce030
pavel <p.varantsou@gmail.com>
2013-09-04 11:05:48 +0300

    Fixed navigation

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,35 @@
 (function ($, router) {
     router('go/:slide', function (slide) {
+        var slide = $('#' + slide);
         $('.slide.current').removeClass('current');
-        $('#' + slide).addClass('current');
+        slide.addClass('current');
+        if (slide.next('.slide').length) {
+            $('#next').removeClass('disabled');
+        } else {
+            $('#next').addClass('disabled');
+        }
+        if (slide.prev('.slide').length) {
+            $('#prev').removeClass('disabled');
+        } else {
+            $('#prev').addClass('disabled');
+        }
     });
+    function next() {
+        var current = $('.slide.current'),
+            next = current.next('.slide');
+        if (next.length) router('go/' + next.attr('id'));
+    }
+    function prev() {
+        var current = $('.slide.current'),
+            prev = current.prev('.slide');
+        if (prev.length) router('go/' + prev.attr('id'));
+    }
+    function handler(el) {
+        return function () { el.trigger('click'); };
+    }
+    $('footer').on('click', '#next:not(".disabled")', next);
+    $('footer').on('click', '#prev:not(".disabled")', prev);
+    Mousetrap.bind('right', handler($('#next')));
+    Mousetrap.bind('left', handler($('#prev')));
+    Mousetrap.bind('ctrl+alt+h', function () { router('go/home'); });
 }(jQuery, routie));
